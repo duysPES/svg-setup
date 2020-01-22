051a66d7e09a6e09f9b22eb55e55f531ca91a77e
Duan Uys <38223595+duysPES@users.noreply.github.com>
2020-01-22 13:18:04 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt update --fix-missing && sudo apt upgrade -y
 
 # download appropriate apt packages
-sudo apt install virtualenv python3.7 python3-tk nodm xserver-xorg xinit openbox emacs -y
+sudo apt install build-essential virtualenv python3.7 python3-tk nodm xserver-xorg xinit openbox emacs -y
 
 # create virtualenv and set python variables
 virtualenv --python=python3.7 ~/py
@@ -18,11 +18,37 @@ py="/home/pi/py/bin/python"
 pip="/home/pi/py/bin/pip"
 
 # install appropriate pip package
-$pip install pyserial pysimplegui ipython 
+$pip install pyserial pysimplegui ipython cython
 
 # clone latest svg
 git clone https://github.com/duysPES/shooting-verification-gui svg
 
-# need to add a test here to make sure all py libraries are good to go.
+CRED_PATH="/home/pi/svg/pysrc"
+CRED_FNAME="credentials"
+CRED_USERNAME="duanuys"
+CRED_PASSWORD="Dawie2018"
+PY_VERSION="3"
+PYTHONLIB="python3.7m"
 
+# write native python func that checks credentials
+printf "%s\n" \
+  "def check_credentials(name, password):" \
+  "    if name != '${CRED_USERNAME}':" \
+  "        return False" \
+  "    if password != '${CRED_PASSWORD}':" \
+  "        return False" \
+  "    return True" \
+  > $CRED_PATH/$CRED_FNAME.py
+  
+  # use cython to convert to *.c
+  $py -m cython $CRED_PATH/$CRED_FNAME -$PY_VERSION
+  
+  
+  # now actually compile it to a .so file
+  gcc -shared -fPIC `pkg-config --cflags ${PYTHONLIB}` $CRED_FNAME.c -o $CRED_FNAME.so
+  
+  # now clean up files that we don't need.
+  rm $CRED_PATH/$CRED_FNAME.*
+
+  
 
